86cebd3df62d716c0cb4afe738889955b9a62379
Navin136 <nkwhitehat@gmail.com>
2023-03-01 19:20:18 +0530

    Changes for TRB_clang V17

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# cd To An Absolute Path
-cd /tmp/rom
-
 # export sync start time
 export TZ=$TZ
 
@@ -52,16 +49,16 @@ echo "Pulling recent changes"
 cd $WORK_DIR/kernel && git pull
 cd ../
 else
-git clone --depth=1 https://github.com/navin136/android_kernel_asus_X00TD $WORK_DIR/kernel
+git clone --depth=1 https://github.com/navin136/kernel_asus_sdm660 $WORK_DIR/kernel
 fi
-if [ -d $WORK_DIR/toolchains/trb_clang-14 ]
+if [ -d $WORK_DIR/toolchains/trb_clang-17 ]
 then
 echo "clang dir exists"
 else
 mkdir $WORK_DIR/toolchains
 cd $WORK_DIR/toolchains
-wget https://hyper.navin136.workers.dev/0:/trbclang/trb_clang-14.zip
-unzip trb_clang-14.zip 
+wget https://gitlab.com/varunhardgamer/trb_clang/-/archive/17/trb_clang-17.zip
+unzip trb_clang-17.zip 
 fi
 cd $WORK_DIR/kernel
 
@@ -73,14 +70,14 @@ DISTRO=$(source /etc/os-release && echo $NAME)
 CORES=$(nproc --all)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 COMMIT_LOG=$(git log --oneline -n 1)
-COMPILER=$($WORK_DIR/toolchains/trb_clang-14/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+COMPILER=$($WORK_DIR/toolchains/trb_clang-17/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 
 #Starting Compilation
 msg "<b>========VELOCITY Kernel========</b>%0A<b>Hey Navin!! Kernel Build Triggered !!</b>%0A<b>Device: </b><code>$DEVICE</code>%0A<b>Kernel Version: </b><code>$VERSION</code>%0A<b>Date: </b><code>$DATE</code>%0A<b>Host Distro: </b><code>$DISTRO</code>%0A<b>Host Core Count: </b><code>$CORES</code>%0A<b>Compiler Used: </b><code>$COMPILER</code>%0A<b>Branch: </b><code>$BRANCH</code>%0A<b>Last Commit: </b><code>$COMMIT_LOG</code>"
 BUILD_START=$(date +"%s")
 export ARCH=arm64
 export SUBARCH=arm64
-export PATH="$WORK_DIR/toolchains/trb_clang-14/bin/:$PATH"
+export PATH="$WORK_DIR/toolchains/trb_clang-17/bin/:$PATH"
 cd $WORK_DIR/kernel
 make clean && make mrproper
 make O=out X00TD_defconfig
